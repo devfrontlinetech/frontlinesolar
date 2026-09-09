34cd9d8c99211b05ab4a75291f2a15eab457ffdc
sumesh <sumesh@frontlinetech.in>
2026-09-09 16:01:33 +0530

    added

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -54,7 +54,7 @@ TableFooter.displayName = 'TableFooter';
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
+>(({ className, ...props }, ref) => ( 
   <tr
     ref={ref}
     className={cn(
